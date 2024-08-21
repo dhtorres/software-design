67a7c57c7870459a18d7912f8841bffe1f37a544
Daniel Torres <>
2024-08-21 15:08:56 -0400

    partial: refactor

--- src/app/login/token.model.ts
+++ src/app/login/token.model.ts
@@ -1,14 +1,13 @@
 import * as jwt from 'jsonwebtoken';
+import { JWT } from '../../environment/environment';
 
 export class Token {
-    private key = 'super-key-secreta';
-
     public sign(user: IToken) {
-        return jwt.sign(user, this.key);
+        return jwt.sign(user, JWT.KEY);
     }
 
     public decode(token: string) {
-        return jwt.verify(token, this.key);
+        return jwt.verify(token, JWT.KEY);
     }
 }
 
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -4,3 +4,7 @@ dotenv.config();
 export const APPLICATION = {
     PORT: process.env.PORT,
 };
+
+export const JWT = {
+    KEY: process.env.JWT_KEY,
+};
